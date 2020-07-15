95ac0fe1f9bfcf21adbb49da4209fa41ecb10ef8
vnapuri <vladnapuri@gmail.com>
2020-07-14 22:04:32 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,28 @@
 # Make your shoe class here!
+require 'pry'
+
+class Shoe
+    
+    attr_accessor :color, :size, :material, :condition
+    #attr_reader :brand
+    
+    def initialize(brand)
+      @brand = brand
+    end 
+
+    def brand
+      @brand
+    end
+
+    def cobble
+      puts "Your shoe is as good as new!"
+      @condition = "new"
+    end 
+
+    def condition
+      @condition
+    end
+end
+
+#cortez = Shoe.new("Nike", "red", "10", "suede", "new")
+
